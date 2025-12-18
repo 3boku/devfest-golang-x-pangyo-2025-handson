335366172480481a3b16f62182b5bacf050e0b73
3boku <a24746440@gmail.com>
2025-12-18 21:11:15 +0900

    feat: 기본 웹 서버 및 프론트엔드 구조 설정
    - Gin 웹 프레임워크를 사용한 기본 서버 설정
    - HTML 템플릿 (index.html) 생성
    - CSS 스타일 (크리스마스 테마) 추가
    - JavaScript 기본 구조 추가

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+
+	// 정적 파일 서빙
+	r.Static("/static", "./static")
+
+	// HTML 템플릿 로드
+	r.LoadHTMLGlob("templates/*")
+
+	// 메인 페이지
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	// 서버 시작
+	log.Println("🎄 크리스마스 카드 생성기 서버 시작: http://localhost:8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("서버 시작 실패:", err)
+	}
 }
